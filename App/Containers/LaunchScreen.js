6de91360807845a16ab66a00f3046d018264bad8
Faisal Bin Basha <faisalbasha1982@gmail.com>
2018-04-07 11:44:27 +0400

    first commit

--- App/Containers/LaunchScreen.js
+++ App/Containers/LaunchScreen.js
@@ -8,22 +8,28 @@ import styles from './Styles/LaunchScreenStyles'
 export default class LaunchScreen extends Component {
   render () {
     return (
-      <View style={styles.mainContainer}>
-        <Image source={Images.background} style={styles.backgroundImage} resizeMode='stretch' />
-        <ScrollView style={styles.container}>
-          <View style={styles.centered}>
-            <Image source={Images.launch} style={styles.logo} />
-          </View>
+      <Container>
+      <Header />
+      <Content>
 
-          <View style={styles.section} >
-            <Image source={Images.ready} />
-            <Text style={styles.sectionText}>
-              This probably isn't what your app is going to look like. Unless your designer handed you this screen and, in that case, congrats! You're ready to ship. For everyone else, this is where you'll see a live preview of your fully functioning app using Ignite.
-            </Text>
-          </View>
+        <View style={styles.centered}>
+          <Image source={Images.launch} style={styles.logo} />
+        </View>
 
-        </ScrollView>
-      </View>
+        <Form>
+          <Item fixedLabel>
+            <Label>Username</Label>
+            <Input />
+          </Item>
+          <Item fixedLabel last>
+            <Label>Password</Label>
+            <Input />
+          </Item>
+        </Form>
+      </Content>
+    </Container>
     )
   }
 }
+
+
